adbe15df4275c3f1d6b65e3731852a483b3ccc38
madkilin <mad.kilin@gmail.com>
2023-09-29 05:49:15 +0700

    build: react app

--- src/components/atoms/title/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Tittle = ({children, color='black', font='regular', size='sm'}) => {
-  return (
-    <h1 className={`text-${size}  font-${font} text-${color} md:tracking-wider`} >{children}</h1>
-  )
-}
-
-export default Tittle
